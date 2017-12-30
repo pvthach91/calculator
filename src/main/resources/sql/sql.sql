586648f8584be6572c4bf42c48d2fde14558f709
pvthach91 <pvthach91@gmail.com>
2017-12-31 00:01:01 +0700

    Refactor code

--- src/main/resources/sql/sql.sql
+++ src/main/resources/sql/sql.sql
@@ -6,9 +6,9 @@ CREATE TABLE user (
   username VARCHAR(30) NOT NULL,
   password text NOT NULL,
   PRIMARY KEY (username)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE content (
+CREATE TABLE history (
   id int(20) NOT NULL AUTO_INCREMENT,
   content text NOT NULL,
   date date NOT NULL,
