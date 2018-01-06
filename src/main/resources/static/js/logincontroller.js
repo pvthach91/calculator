19852523b5a8ff16d159f843d8895ea59df08955
pvthach91 <pvthach91@gmail.com>
2018-01-06 09:43:23 +0700

    Update front end

--- src/main/resources/static/js/logincontroller.js
+++ src/main/resources/static/js/logincontroller.js
@@ -4,43 +4,59 @@ app.controller('logincontroller', function($scope, $http, $location, $window) {
             $scope.loginResultMessage = "";
             var username = $scope.username;
             var password = $scope.password;
-            var isValid = true;
-            if (username == null || username.length == 0) {
-                $scope.usernameError = "Please enter username";
-                isValid = false;
-            } else {
-                $scope.usernameError = "";
-            }
-            if (password == null || password.length == 0) {
-                $scope.passwordError = "Please enter password";
-                isValid = false;
-            } else {
-                $scope.passwordError = "";
-            }
+            var isValid = $scope.validateInput(username, password);
+
             if (isValid) {
-                var url = $location.absUrl();
-                var config = {
-                    headers : {
-                        'Accept': 'application/json'
-                    }
-                }
-
-                var data = {
-                    username: $scope.username,
-                    password: $scope.password
-                };
-
-                $http.post(url, data, config).then(function (response) {
-                    var user = response.data.username;
-                    $window.sessionStorage.setItem("user",user);
-                    var url = $location.protocol() + "://" + location.host;
-                    $window.location.href = url;
-                }, function error(response) {
-                    $scope.loginResultMessage = response.data.message;
-                });
+                $scope.login(username, password);
             }
-
 	}
+
+    $scope.validateInput = function(username, password){
+        var isValid = true;
+        if (username == null || username.length == 0) {
+            $scope.usernameError = "Please enter username";
+            isValid = false;
+        } else {
+            $scope.usernameError = "";
+        }
+        if (password == null || password.length == 0) {
+            $scope.passwordError = "Please enter password";
+            isValid = false;
+        } else {
+            $scope.passwordError = "";
+        }
+
+        return isValid;
+    }
+
+    $scope.login = function(username, password){
+        var url = $location.absUrl();
+        var authorization = "Basic " + btoa(username + ":" + password);
+        var config = {
+            headers : {
+                'Accept': 'application/json',
+                'authorization' : authorization
+            }
+        }
+        $http.defaults.headers.common.Authorization = authorization;
+        $http.post(url, config).then(function (response) {
+            $scope.handleLoginSuccess(authorization);
+        }, function error(response) {
+            $scope.loginResultMessage = response.data.message;
+        });
+    }
+
+    $scope.handleLoginSuccess = function(authorization){
+        $window.sessionStorage.setItem("authorization",authorization);
+        $window.sessionStorage.setItem("authenticated",true);
+        $scope.goToCalculatorPage();
+    }
+
+    $scope.goToCalculatorPage = function(){
+        var url = $location.protocol() + "://" + location.host;
+        $window.location.href = url;
+    }
+
     $scope.loginResultMessage = "";
 
     $scope.signup = function(){
