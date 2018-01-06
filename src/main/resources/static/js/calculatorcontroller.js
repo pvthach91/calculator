f544fbf40e2e6533be86813b074ab094be942950
pvthach91 <pvthach91@gmail.com>
2018-01-06 11:32:12 +0700

    Update front end

--- src/main/resources/static/js/calculatorcontroller.js
+++ src/main/resources/static/js/calculatorcontroller.js
@@ -1,21 +1,29 @@
-var app = angular.module('app', []).config(function($httpProvider){
-    $httpProvider.detail.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-});
+var app = angular.module('app', []);
 app.controller('calculatorcontroller', function($scope, $http, $location, $window) {
-    var user = $scope.name = $window.sessionStorage.getItem("user");
-    if (user == null || user.length == 0){
+    var authenticated = $scope.name = $window.sessionStorage.getItem("authenticated");
+    if (authenticated == null || authenticated == 'false' ){
         $window.location.href = 'login';
     }
 
+    var authorization = $window.sessionStorage.getItem("authorization");
+    var data = "";
+    var calculationRequest = "";
+
 	$scope.submitForm = function(){
-	    var operation= $scope.operation;
+        var operation= $scope.operation;
         var firstNumber = $scope.firstNumber;
         var secondNumber = $scope.secondNumber;
+
+        var isValid = $scope.validateInput(firstNumber, secondNumber, operation);
+
+        if (isValid) {
+            $scope.calculate();
+        }
+	}
+
+    $scope.validateInput = function(firstNumber, secondNumber, operation){
         var isValid = true;
         var needCheckSecondNumber = true;
-        var calculationRequest = "";
-        var data = "";
-        $scope.calculateResultMessage = "";
 
         if (firstNumber == null || firstNumber.length == 0 || isNaN(firstNumber)) {
             $scope.firstNumberError = "Please enter number";
@@ -33,14 +41,12 @@ app.controller('calculatorcontroller', function($scope, $http, $location, $windo
                 calculationRequest = "/calculateOneParam";
                 needCheckSecondNumber = false;
                 data = {
-                    user: user,
                     type: operation,
                     param: firstNumber
                 };
             } else {
                 calculationRequest = "/calculateTwoParam";
                 data = {
-                    user: user,
                     type: operation,
                     firstParam: firstNumber,
                     secondParam: secondNumber
@@ -56,28 +62,32 @@ app.controller('calculatorcontroller', function($scope, $http, $location, $windo
             }
         }
 
-        if (isValid) {
-            var url = $location.absUrl() + calculationRequest;
-            var config = {
-                headers : {
-                    'Accept': 'application/json'
-                }
-            }
+        return isValid;
+    }
 
-            $http.post(url, data, config).then(function (response) {
-                $scope.calculateResultMessage = response.data;
-            }, function error(response) {
-                $scope.calculateResultMessage = response.data.message;
-            });
+    $scope.calculate = function(){
+        var url = $location.absUrl() + calculationRequest;
+        var config = {
+            headers : {
+                'Accept': 'application/json',
+                'authorization' : authorization
+            }
         }
-	}
+        $http.defaults.headers.common.Authorization = authorization;
+        $http.post(url, data, config).then(function (response) {
+            $scope.calculateResultMessage = response.data;
+        }, function error(response) {
+            $scope.calculateResultMessage = response.data.message;
+        });
+    }
 
     $scope.viewHistory = function(){
         $window.location.href = '/history';
     }
 
     $scope.logout = function(){
-        $window.sessionStorage.removeItem("user")
+        $window.sessionStorage.removeItem("authorization")
+        $window.sessionStorage.setItem("authenticated",false);
         $window.location.href = '/login';
     }
 });
--- src/main/resources/static/js/historycontroller.js
+++ src/main/resources/static/js/historycontroller.js
@@ -1,33 +1,36 @@
 var app = angular.module('app', []);
 app.controller('historycontroller', function($scope, $http, $location, $window) {
-    var user = $scope.name = $window.sessionStorage.getItem("user");
-    if (user == null || user.length == 0){
-        var url = $location.protocol() + "://" + location.host +"/login";
-        $window.location.href = url;
+    var authenticated = $window.sessionStorage.getItem("authenticated");
+    if (authenticated == null || authenticated == 'false' ){
+        $window.location.href = 'login';
     }
 
+    var authorization = $scope.name = $window.sessionStorage.getItem("authorization");
+
 	$scope.refresh = function(){
-        var url = $location.absUrl();
+        var url = $location.protocol() + "://" + location.host + "/histories";
         var config = {
             headers : {
-                'Accept': 'application/json'
+                'Accept': 'application/json',
+                'authorization' : authorization
             }
         }
-
-        $http.post(url, user, config).then(function (response) {
+        $http.defaults.headers.common.Authorization = authorization;
+        $http.get(url, config).then(function (response) {
             $scope.histories = response.data;
         }, function error(response) {
             $scope.historyResultMessage = response.data.message;
         });
 	}
 
-    $scope.calculate = function(){
+    $scope.goToCalculatorPage = function(){
         var url = $location.protocol() + "://" + location.host;
         $window.location.href = url;
     }
 
     $scope.logout = function(){
-        $window.sessionStorage.removeItem("user")
+        $window.sessionStorage.removeItem("authorization")
+        $window.sessionStorage.setItem("authenticated",false);
         var url = $location.protocol() + "://" + location.host + "/login";
         $window.location.href = url;
     }
